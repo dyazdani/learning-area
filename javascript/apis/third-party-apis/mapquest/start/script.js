b0adaf464b4045d6fd62eceab41428a6d4e90198
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-03-22 12:33:53 -0700

    Added more map features

--- javascript/apis/third-party-apis/mapquest/start/script.js
+++ javascript/apis/third-party-apis/mapquest/start/script.js
@@ -3,6 +3,21 @@ L.mapquest.key = 's9X9CCTBQY7NOlO90LZpuFygklRMsQ10';
 // 'map' refers to a <div> element with the ID map
 const map = L.mapquest.map('map', {
   center: [53.480759, -2.242631],
-  layers: L.mapquest.tileLayer('map'),
+  layers: L.mapquest.tileLayer('dark'),
   zoom: 12
 });
+
+map.addControl(L.mapquest.control({ position: 'topright' }));
+map.addControl(L.mapquest.satelliteControl());
+
+L.marker([53.480759, -2.242631], {
+  icon: L.mapquest.icons.marker({
+    primaryColor: '#22407F',
+    secondaryColor: '#3B5998',
+    shadow: true,
+    size: 'md',
+    symbol: 'A'
+  })
+})
+.bindPopup('This is Manchester!')
+.addTo(map);
